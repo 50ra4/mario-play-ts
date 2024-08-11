f1514996e38552ab975c3646bb5507078c3ccfd7
50ra4 <50ra4.dev.dev@gmail.com>
2024-08-11 12:37:46 +0900

    feat: draw mario course

--- src/presentation/pages/TopPage/TopPage.tsx
+++ src/presentation/pages/TopPage/TopPage.tsx
@@ -1,22 +1,56 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+
+const COURSE = [
+  '                                               ',
+  '                                               ',
+  '                                               ',
+  '             ccc                               ',
+  '             ccc                               ',
+  '                                               ',
+  '                                               ',
+  '                                               ',
+  '                                               ',
+  '              q   bpbpb                        ',
+  '  m                                            ',
+  ' mmm                                           ',
+  'mmmmm    ttttmm                                ',
+  'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb  bbbbbbbbbbbbb',
+  'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb  bbbbbbbbbbbbb',
+];
+
+const COURSE_ITEM = {
+  ' ': '　',
+  m: '🔺',
+  b: '🟫',
+  t: '🌴',
+  q: '❓',
+  p: '🪣',
+  c: '☁️',
+} as const;
+
+const VISIBLE_SCREEN_SIZE = {
+  width: 16,
+  height: 16,
+};
+
+const replaceCourseItems = (row: string) =>
+  Object.entries(COURSE_ITEM).reduce(
+    (acc, [key, item]) => acc.replaceAll(key, item),
+    row,
+  );
+
+const drawScreen = () =>
+  COURSE.slice(0, VISIBLE_SCREEN_SIZE.height)
+    .map((row) => row.slice(0, VISIBLE_SCREEN_SIZE.width))
+    .map(replaceCourseItems)
+    .join('\n');
 
 function TopPage() {
   return (
     <main>
-      <h2>TopPage</h2>
-      <div>TopPage component</div>
-      <ul>
-        <li>
-          <Link to="/1">Page1</Link>
-        </li>
-        <li>
-          <Link to="/2">Page2</Link>
-        </li>
-        <li>
-          <Link to="/3">Page3</Link>
-        </li>
-      </ul>
+      <div style={{ backgroundColor: 'aliceblue', whiteSpace: 'pre-wrap' }}>
+        {drawScreen()}
+      </div>
     </main>
   );
 }
